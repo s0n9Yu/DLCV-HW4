7c233149b8a0460226f42c86e195b15ac43370da
楊宗儒 <s0n9yu@gpu10.cplab.cs.nycu.edu.tw>
2025-05-25 14:08:36 +0000

    linting

--- inference.py
+++ inference.py
@@ -1,80 +1,87 @@
 import argparse
-import subprocess
 from tqdm import tqdm
 import numpy as np
-
 import torch
 from torch.utils.data import DataLoader
-import os
-import torch.nn as nn 
+import torch.nn as nn
 
-#from utils.dataset_utils import DenoiseTestDataset, DerainDehazeDataset
-#from utils.val_utils import AverageMeter, compute_psnr_ssim
-#from utils.image_io import save_image_tensor
 from net.model import PromptIR
-
 import lightning.pytorch as pl
-import torch.nn.functional as F
-
-import matplotlib.pyplot as plt
 
 from utils.dataset_ours import OursDataset
 
 trainset = OursDataset(path="data_ours/train", patchsize=256)
 
+
 class PromptIRModel(pl.LightningModule):
     def __init__(self):
         super().__init__()
         self.net = PromptIR(decoder=True)
-        self.loss_fn  = nn.L1Loss()
-    
-    def forward(self,x):
+        self.loss_fn = nn.L1Loss()
+
+    def forward(self, x):
         return self.net(x)
-    
+
     def training_step(self, batch, batch_idx):
         # training_step defines the train loop.
         # it is independent of forward
         ([clean_name, de_id], degrad_patch, clean_patch) = batch
         restored = self.net(degrad_patch)
 
-        loss = self.loss_fn(restored,clean_patch)
+        loss = self.loss_fn(restored, clean_patch)
         # Logging to TensorBoard (if installed) by default
         self.log("train_loss", loss)
         return loss
-    
-    def lr_scheduler_step(self,scheduler,metric):
+
+    def lr_scheduler_step(self, scheduler, metric):
         scheduler.step(self.current_epoch)
-        lr = scheduler.get_lr()
-    
+        # lr = scheduler.get_lr()
+
     def configure_optimizers(self):
+        ...
+        '''
         optimizer = optim.AdamW(self.parameters(), lr=2e-4)
-        scheduler = LinearWarmupCosineAnnealingLR(optimizer=optimizer,warmup_epochs=15,max_epochs=150)
-
-        return [optimizer],[scheduler]
-
+        scheduler = LinearWarmupCosineAnnealingLR(
+            optimizer=optimizer,
+            warmup_epochs=15,
+            max_epochs=150
+        )
+        '''
+        # return [optimizer],[scheduler]
 
 
 parser = argparse.ArgumentParser()
 # Input Parameters
-parser.add_argument('--ckpt_path', type=str, default="model.ckpt", help='checkpoint save path')
+parser.add_argument(
+    '--ckpt_path',
+    type=str,
+    default="model.ckpt",
+    help='checkpoint save path'
+)
 opt = parser.parse_args()
 
-net  = PromptIRModel.load_from_checkpoint(opt.ckpt_path)
+net = PromptIRModel.load_from_checkpoint(opt.ckpt_path)
 net = net.cuda()
 net.eval()
 
 dataset = OursDataset(path="data_ours/test", inference=True)
 
-testloader = DataLoader(dataset, batch_size=1, pin_memory=True, shuffle=False, num_workers=0)
+testloader = DataLoader(
+    dataset,
+    batch_size=1,
+    pin_memory=True,
+    shuffle=False,
+    num_workers=0
+)
 img_dict = {}
 with torch.no_grad():
     for (degrad_patch, image_name) in tqdm(testloader):
         print(image_name[0])
         print("input max", degrad_patch.max())
-        degrad_patch= degrad_patch.cuda()
+        degrad_patch = degrad_patch.cuda()
         restored = net(degrad_patch).squeeze(0).cpu().numpy()
         # Rearrange to (3, H, W)
-        #img_array = np.transpose(restored, (2, 0, 1))
+        # img_array = np.transpose(restored, (2, 0, 1))
         img_array = restored
         img_array = np.clip(img_array, a_min=0, a_max=1)
         img_array = img_array * 255
@@ -93,10 +100,8 @@ with torch.no_grad():
         plt.show()
         '''
 
-
         # Add to dictionary
         img_dict[image_name[0]] = img_array
-
 
 # Save to .npz file
 output_npz = 'pred.npz'
--- train.py
+++ train.py
@@ -1,19 +1,12 @@
-import subprocess
-from tqdm import tqdm
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
-from utils.dataset_utils import PromptTrainDataset
 from net.model import PromptIR
-from utils.schedulers import LinearWarmupCosineAnnealingLR
-import numpy as np
-#import wandb
 from options import options as opt
 import lightning.pytorch as pl
-from lightning.pytorch.loggers import WandbLogger,TensorBoardLogger
+from lightning.pytorch.loggers import WandbLogger, TensorBoardLogger
 from lightning.pytorch.callbacks import ModelCheckpoint
 
 from utils.dataset_ours import OursDataset
@@ -23,30 +16,29 @@ class PromptIRModel(pl.LightningModule):
     def __init__(self):
         super().__init__()
         self.net = PromptIR(decoder=True)
-        self.loss_fn  = nn.L1Loss()
+        self.loss_fn = nn.L1Loss()
         self.l2loss = nn.MSELoss()
         self.val_outputs = []
-    
-    def forward(self,x):
+
+    def forward(self, x):
         return self.net(x)
-    
+
     def training_step(self, batch, batch_idx):
         # training_step defines the train loop.
         # it is independent of forward
         ([clean_name, de_id], degrad_patch, clean_patch) = batch
         restored = self.net(degrad_patch)
 
-        #loss = (self.loss_fn(restored,clean_patch) + self.l2loss(restored, clean_patch)) / 2
-        loss = self.loss_fn(restored,clean_patch)
-        # Logging to TensorBoard (if installed) by default
+        loss = (self.loss_fn(restored, clean_patch)
+                + self.l2loss(restored, clean_patch)) / 2
         self.log("train_loss", loss)
         return loss
+
     def validation_step(self, batch, batch_idx):
         ([clean_name, de_id], degrad_patch, clean_patch) = batch
         restored = self.net(degrad_patch)
 
-        loss = nn.MSELoss()(restored,clean_patch)
-        #self.log("val_loss", loss, prog_bar=True)
+        loss = nn.MSELoss()(restored, clean_patch)
         self.val_outputs.append(loss)  # store for epoch-end processing
 
     def on_validation_epoch_end(self):
@@ -54,48 +46,68 @@ class PromptIRModel(pl.LightningModule):
         self.log("val_loss", avg_val_loss, prog_bar=True, sync_dist=True)
         self.val_outputs.clear()  # reset for next epoch
 
-    
-    def lr_scheduler_step(self,scheduler,metric):
+    def lr_scheduler_step(self, scheduler, metric):
         scheduler.step(self.current_epoch)
-        lr = scheduler.get_lr()
-    
+        # lr = scheduler.get_lr()
+
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=2e-4)
-        #scheduler = LinearWarmupCosineAnnealingLR(optimizer=optimizer,warmup_epochs=15,max_epochs=500, eta_min=1e-6)
         scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=500)
 
-        return [optimizer],[scheduler]
-
-
-
-
+        return [optimizer], [scheduler]
 
 
 def main():
     print("Options")
     print(opt)
     if opt.wblogger is not None:
-        logger  = WandbLogger(project=opt.wblogger,name=f"PromptIR-Train-{opt.expname}")
+        logger = WandbLogger(
+            project=opt.wblogger,
+            name=f"PromptIR-Train-{opt.expname}"
+        )
     else:
-        logger = TensorBoardLogger(save_dir = "logs/")
+        logger = TensorBoardLogger(save_dir="logs/")
 
-    #trainset = PromptTrainDataset(opt)
     trainset = OursDataset(path="data_ours/train", patchsize=128)
     validset = OursDataset(path="data_ours/valid", patchsize=128)
-    checkpoint_callback = ModelCheckpoint(dirpath = opt.ckpt_dir,every_n_epochs = 1,save_top_k=-1, filename='{epoch}-'+f'{opt.expname}')
-    trainloader = DataLoader(trainset, batch_size=opt.batch_size, pin_memory=True, shuffle=True,
-                             drop_last=True, num_workers=opt.num_workers)
-    validloader = DataLoader(validset, batch_size=opt.batch_size, pin_memory=True, shuffle=False,
-                             drop_last=True, num_workers=opt.num_workers)
-    
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=opt.ckpt_dir,
+        every_n_epochs=1,
+        save_top_k=-1,
+        filename='{epoch}-'+f'{opt.expname}'
+    )
+    trainloader = DataLoader(
+        trainset,
+        batch_size=opt.batch_size,
+        pin_memory=True,
+        shuffle=True,
+        drop_last=True,
+        num_workers=opt.num_workers
+    )
+    validloader = DataLoader(
+        validset,
+        batch_size=opt.batch_size,
+        pin_memory=True,
+        shuffle=False,
+        drop_last=True,
+        num_workers=opt.num_workers
+    )
+
     model = PromptIRModel()
-    
-    trainer = pl.Trainer( max_epochs=opt.epochs,accelerator="gpu",devices=1,strategy="ddp_find_unused_parameters_true",logger=logger,callbacks=[checkpoint_callback])
-    trainer.fit(model=model, train_dataloaders=trainloader, val_dataloaders=validloader)
+
+    trainer = pl.Trainer(
+        max_epochs=opt.epochs,
+        accelerator="gpu",
+        devices=1,
+        strategy="ddp_find_unused_parameters_true",
+        logger=logger, callbacks=[checkpoint_callback]
+    )
+    trainer.fit(
+        model=model,
+        train_dataloaders=trainloader,
+        val_dataloaders=validloader
+    )
 
 
 if __name__ == '__main__':
     main()
-
-
-
